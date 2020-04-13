333588b0628f8239090ea12b78791347a2ce0de2
charlesinto <charles.onuorah@yahoo.com>
2020-04-13 16:06:23 +0100

    Moved to dependencies

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
-import cors from 'cors'
+import cors from 'cors';
 import covidRoute from './routes/covidRoute';
 
 
@@ -19,8 +19,6 @@ app.use('/api/v1/on-covid-19', covidRoute);
 app.get('/', (req, res) => {
   return res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
-
 
 app.listen(PORT, () => {
   console.log('app is listening on port: ', PORT);
